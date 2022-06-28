614a83f41c72c9a5198c0b258bfbe57b34dced4f
Russ Allbery <rra@lsst.org>
2022-06-28 11:21:00 -0700

    Use per-thread Google storage clients
    We're seeing problems where crawlspace locks up and stops answering
    any requests after a small amount of traffic.  The backtrace is a
    timeout in the handler.  This may be due to reusing Google Cloud
    Storage clients across threads, resulting in cross-thread confusion
    about responses to requests.  Try using per-thread instances via
    context variables to see if that helps.

--- src/crawlspace/dependencies/gcs.py
+++ src/crawlspace/dependencies/gcs.py
@@ -2,11 +2,13 @@
 
 from __future__ import annotations
 
-from typing import Optional
+from contextvars import ContextVar
 
 from google.cloud import storage
 
 from ..config import config
+
+_GCS_CLIENT: ContextVar[storage.Client] = ContextVar("_GCS_CLIENT")
 
 __all__ = [
     "GCSClientDependency",
@@ -17,14 +19,13 @@ __all__ = [
 class GCSClientDependency:
     """Provides a `google.cloud.storage.Client` as a dependency."""
 
-    def __init__(self) -> None:
-        self.gcs: Optional[storage.Client] = None
-
     async def __call__(self) -> storage.client.Client:
         """Return the cached `google.cloud.storage.Client`."""
-        if not self.gcs:
-            self.gcs = storage.Client(project=config.gcs_project)
-        return self.gcs
+        client = _GCS_CLIENT.get(None)
+        if not client:
+            client = storage.Client(project=config.gcs_project)
+            _GCS_CLIENT.set(client)
+        return client
 
 
 gcs_client_dependency = GCSClientDependency()
