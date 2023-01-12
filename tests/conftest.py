e2b8ebcd9dcbd7e8f3fc3a80f2bbaf892c87f10c
Russ Allbery <rra@lsst.org>
2023-01-12 10:01:11 -0800

    Use the GCS mock support from Safir
    Replace our GCS mock support library with the new GCS mock support
    from Safir.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,17 @@
 
 from __future__ import annotations
 
-from typing import AsyncIterator
+from pathlib import Path
+from typing import AsyncIterator, Iterator
 
+import pytest
 import pytest_asyncio
 from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
 from httpx import AsyncClient
+from safir.testing.gcs import MockStorageClient, patch_google_storage
 
 from crawlspace import main
-
-from .support.gcs import patch_google_storage
 
 
 @pytest_asyncio.fixture
@@ -22,8 +23,7 @@ async def app() -> AsyncIterator[FastAPI]:
     events are sent during test execution.
     """
     async with LifespanManager(main.app):
-        with patch_google_storage():
-            yield main.app
+        yield main.app
 
 
 @pytest_asyncio.fixture
@@ -31,3 +31,8 @@ async def client(app: FastAPI) -> AsyncIterator[AsyncClient]:
     """Return an ``httpx.AsyncClient`` configured to talk to the test app."""
     async with AsyncClient(app=app, base_url="https://example.com/") as client:
         yield client
+
+
+@pytest.fixture
+def mock_gcs() -> Iterator[MockStorageClient]:
+    yield from patch_google_storage(path=Path(__file__).parent / "files")
