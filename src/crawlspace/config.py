62645a50bec5885f5e50c81214c475e5e4d293ae
Dan Fuchs <danfuchs@slac.stanford.edu>
2025-06-03 15:42:06 -0500

    Convert settings to a pydantic BaseSettings

--- src/crawlspace/config.py
+++ src/crawlspace/config.py
@@ -2,17 +2,20 @@
 
 from __future__ import annotations
 
-import os
-from dataclasses import dataclass
+from typing import Annotated
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 __all__ = ["Configuration", "config"]
 
 
-@dataclass
-class Configuration:
+class Configuration(BaseSettings):
     """Configuration for crawlspace."""
 
-    cache_max_age: int = int(os.getenv("CRAWLSPACE_CACHE_MAX_AGE", "3600"))
+    cache_max_age: Annotated[
+        int, Field(default=3600, validation_alias="CRAWLSPACE_CACHE_MAX_AGE")
+    ]
     """Length of time in seconds for which browsers should cache results.
 
     The default is one hour.  Set this shorter for testing when the content
@@ -21,43 +24,53 @@ class Configuration:
     environment variable.
     """
 
-    gcs_project: str = os.getenv("CRAWLSPACE_PROJECT", "None")
+    gcs_project: Annotated[
+        str, Field(default="None", validation_alias="CRAWLSPACE_PROJECT")
+    ]
     """The GCS project from which to serve files.
 
     Set with the ``CRAWLSPACE_PROJECT`` environment variable.
     """
 
-    gcs_bucket: str = os.getenv("CRAWLSPACE_BUCKET", "None")
+    gcs_bucket: Annotated[
+        str, Field(default="None", validation_alias="CRAWLSPACE_BUCKET")
+    ]
     """The GCS bucket name from which to serve files.
 
     Set with the ``CRAWLSPACE_BUCKET`` environment variable.
     """
 
-    url_prefix: str = os.getenv("CRAWLSPACE_URL_PREFIX", "/api/hips")
+    url_prefix: Annotated[
+        str, Field(default="/api/hips", validation_alias="CRAWLSPACE_URL_PREFIX")
+    ]
     """URL prefix for routes.
 
     Set with the ``CRAWLSPACE_URL_PREFIX`` environment variable.
     """
 
-    name: str = os.getenv("SAFIR_NAME", "crawlspace")
+    name: Annotated[str, Field(default="crawlspace", validation_alias="SAFIR_NAME")]
     """The application's name, which doubles as the root HTTP endpoint path.
 
     Set with the ``SAFIR_NAME`` environment variable.
     """
 
-    profile: str = os.getenv("SAFIR_PROFILE", "production")
+    profile: Annotated[
+        str, Field(default="production", validation_alias="SAFIR_PROFILE")
+    ]
     """Application run profile: "development" or "production".
 
     Set with the ``SAFIR_PROFILE`` environment variable.
     """
 
-    logger_name: str = os.getenv("SAFIR_LOGGER", "crawlspace")
+    logger_name: Annotated[
+        str, Field(default="crawlspace", validation_alias="SAFIR_LOGGER")
+    ]
     """The root name of the application's logger.
 
     Set with the ``SAFIR_LOGGER`` environment variable.
     """
 
-    log_level: str = os.getenv("SAFIR_LOG_LEVEL", "INFO")
+    log_level: Annotated[str, Field(default="INFO", validation_alias="SAFIR_LOG_LEVEL")]
     """The log level of the application's logger.
 
     Set with the ``SAFIR_LOG_LEVEL`` environment variable.
