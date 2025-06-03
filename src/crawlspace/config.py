b21c0b8670817bf6fb800c805a3b3e3dce3b8f76
Dan Fuchs <danfuchs@slac.stanford.edu>
2025-06-03 15:42:06 -0500

    DM-51236: Read config from a YAML file
    * Convert config to a Pydantic model
    * Read it from a file instead of env vars

--- src/crawlspace/config.py
+++ src/crawlspace/config.py
@@ -2,67 +2,74 @@
 
 from __future__ import annotations
 
-import os
-from dataclasses import dataclass
+from pathlib import Path
+from typing import Annotated, Self
 
-__all__ = ["Configuration", "config"]
+import yaml
+from pydantic import BaseModel, Field
+
+__all__ = ["Config"]
 
 
-@dataclass
-class Configuration:
+class Config(BaseModel):
     """Configuration for crawlspace."""
 
-    cache_max_age: int = int(os.getenv("CRAWLSPACE_CACHE_MAX_AGE", "3600"))
+    cache_max_age: Annotated[
+        int, Field(default=3600, validation_alias="cacheMaxAge")
+    ]
     """Length of time in seconds for which browsers should cache results.
 
     The default is one hour.  Set this shorter for testing when the content
     may be changing frequently, and longer for production when serving static
-    data that rarely varies.  Set with the ``CRAWLSPACE_CACHE_MAX_AGE``
-    environment variable.
+    data that rarely varies.
     """
 
-    gcs_project: str = os.getenv("CRAWLSPACE_PROJECT", "None")
-    """The GCS project from which to serve files.
+    gcs_project: Annotated[
+        str, Field(default="None", validation_alias="gcsProject")
+    ]
+    """The GCS project from which to serve files."""
 
-    Set with the ``CRAWLSPACE_PROJECT`` environment variable.
-    """
+    gcs_bucket: Annotated[
+        str, Field(default="None", validation_alias="gcsBucket")
+    ]
+    """The GCS bucket name from which to serve files."""
 
-    gcs_bucket: str = os.getenv("CRAWLSPACE_BUCKET", "None")
-    """The GCS bucket name from which to serve files.
+    url_prefix: Annotated[
+        str, Field(default="/api/hips", validation_alias="urlPrefix")
+    ]
+    """URL prefix for routes."""
 
-    Set with the ``CRAWLSPACE_BUCKET`` environment variable.
-    """
+    name: Annotated[str, Field(default="crawlspace", validation_alias="name")]
+    """The application's name, which doubles as the root HTTP endpoint path."""
 
-    url_prefix: str = os.getenv("CRAWLSPACE_URL_PREFIX", "/api/hips")
-    """URL prefix for routes.
+    profile: Annotated[
+        str, Field(default="production", validation_alias="profile")
+    ]
+    """Application run profile: "development" or "production"."""
 
-    Set with the ``CRAWLSPACE_URL_PREFIX`` environment variable.
-    """
+    logger_name: Annotated[
+        str, Field(default="crawlspace", validation_alias="loggerName")
+    ]
+    """The root name of the application's logger."""
 
-    name: str = os.getenv("SAFIR_NAME", "crawlspace")
-    """The application's name, which doubles as the root HTTP endpoint path.
+    log_level: Annotated[
+        str, Field(default="INFO", validation_alias="logLevel")
+    ]
+    """The log level of the application's logger."""
 
-    Set with the ``SAFIR_NAME`` environment variable.
-    """
+    @classmethod
+    def from_file(cls, path: Path) -> Self:
+        """Construct a Configuration object from a configuration file.
 
-    profile: str = os.getenv("SAFIR_PROFILE", "production")
-    """Application run profile: "development" or "production".
+        Parameters
+        ----------
+        path
+            Path to the configuration file in YAML.
 
-    Set with the ``SAFIR_PROFILE`` environment variable.
-    """
-
-    logger_name: str = os.getenv("SAFIR_LOGGER", "crawlspace")
-    """The root name of the application's logger.
-
-    Set with the ``SAFIR_LOGGER`` environment variable.
-    """
-
-    log_level: str = os.getenv("SAFIR_LOG_LEVEL", "INFO")
-    """The log level of the application's logger.
-
-    Set with the ``SAFIR_LOG_LEVEL`` environment variable.
-    """
-
-
-config = Configuration()
-"""Configuration for crawlspace."""
+        Returns
+        -------
+        Config
+            The corresponding `Configuration` object.
+        """
+        with path.open("r") as f:
+            return cls.model_validate(yaml.safe_load(f))
